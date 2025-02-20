c1fa6876c3edd45ff8eb526de7f3791ebbe6a1b1
Rafael Henrique da Silva <31450046+rafaelhalder@users.noreply.github.com>
2025-02-20 00:22:16 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { Input } from "@/components/input";
+import { ArrowRight,Copy } from "lucide-react";
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <Copy/>
+      </IconButton>
+      <Input />
     </main>
   );
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,14 @@
+import { Mail } from "lucide-react";
+import { ComponentProps } from "react";
+interface InputProps extends ComponentProps<'input'> { }
+export function Input(props: InputProps) {
+  return (
+    <div className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2  focus-within:border-gray-100">
+      <span className="text-gray-400 group-focus-within:text-gray-100"><Mail /></span>
+      <input
+        className="flex-1 outline-0"
+        {...props} />
+    </div>
+  )
+
+}
